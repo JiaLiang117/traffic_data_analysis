87b255f51d3763aaa33b4474c6807bdbf9acddf4
Kelvin Low <lowjialiang@hotmail.com>
2019-11-10 19:53:41 +0800

    stuff

--- run.sh
+++ run.sh
@@ -1,2 +1,15 @@
-python mlp/train.py https://aisgaiap.blob.core.windows.net/aiap5-assessment-data/traffic_data.csv models/output.pkl
-python mlp/deploy.py "models/output.pkl" "https://aisgaiap.blob.core.windows.net/aiap5-assessment-data/traffic_data.csv" "model1"
+#!/bin/bash
+
+echo "Run Train? [Y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "Running Train";
+        python mlp/train.py https://aisgaiap.blob.core.windows.net/aiap5-assessment-data/traffic_data.csv models/output.pkl;
+else
+        echo "Not Running Train"
+fi
+
+echo "Which model to deploy? "
+read input
+
+python mlp/deploy.py "models/output.pkl" "https://aisgaiap.blob.core.windows.net/aiap5-assessment-data/traffic_data.csv" $input
